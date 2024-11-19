cd1358f5c8863f7065c3381ff86541918c68176d
bandageslmao <146501103+bandageslmao@users.noreply.github.com>
2024-11-18 18:10:59 -0600

    Update rockbottom.js

--- javascript/rockbottom.js
+++ javascript/rockbottom.js
@@ -1,7 +1,7 @@
  $(window).scroll(function() {   
    if($(window).scrollTop() + $(window).height() == $(document).height()) {
        alert("woah! you hit the bottom of this page! great job!");
-    var x = document.getElementById("myAudio"); 
+    var x = document.getElementById("rockbottom"); 
 
 function playAudio() { 
   x.play(); 
