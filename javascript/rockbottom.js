33e214d75afc705867aaa2f193179156cf5b3552
bandageslmao <146501103+bandageslmao@users.noreply.github.com>
2024-11-18 18:10:38 -0600

    Update rockbottom.js

--- javascript/rockbottom.js
+++ javascript/rockbottom.js
@@ -1,5 +1,13 @@
  $(window).scroll(function() {   
    if($(window).scrollTop() + $(window).height() == $(document).height()) {
        alert("woah! you hit the bottom of this page! great job!");
+    var x = document.getElementById("myAudio"); 
+
+function playAudio() { 
+  x.play(); 
+} 
+
+function pauseAudio() { 
+  x.pause(); 
    }
 });
