92a5d0328a9ef7e5d2c4aab10fee55fe6eea1eae
bandageslmao <146501103+bandageslmao@users.noreply.github.com>
2025-12-05 16:24:51 -0600

    rfty

---
+++ javascript/dir.js
@@ -0,0 +1,5 @@
+document.getElementById('area1').addEventListener('click',function(event){
+    event.preventDefault()
+    document.getElementById('selection').style.display = "none"
+    document.getElementById('chars').style.display = "block"
+})
--- javascript/sounds.js
+++ javascript/sounds.js
@@ -3,9 +3,16 @@ function hover(){
     audio.src = '../sounds/select.wav'
     audio.play().catch(e => alert("Error" + e.message))
 }
+
+function click(){
+    var audio = new Audio
+    audio.src = '../sounds/done.wav'
+    audio.play()
+}
 document.addEventListener('DOMContentLoaded', function() {
     var links = document.querySelectorAll('a')
     links.forEach(link => {
         link.addEventListener('mouseover', hover)
+        link.addEventListener('click', click)
     })
 })
